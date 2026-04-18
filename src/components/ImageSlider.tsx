2cd0b4aa8f768f55893c134650a68a4cfb21042b
Muhammad Saad Khan <rts.advertisinghub@gmail.com>
2026-04-18 18:36:41 +0500

    edited
    Edited images

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -9,7 +9,7 @@ import aviation3 from "@/assets/projects/aviation3.jpeg";
 import aviation4 from "@/assets/projects/aviation4.jpeg";
 import faisal1 from "@/assets/image gallery/faisal.png";
 import faisal2 from "@/assets/projects/rbs.jpeg";
-import billboard1 from "@/assets/projects/billboard1.jpg";
+import billboard1 from "@/assets/projects/Billboardrental.jpeg";
 import billboard2 from "@/assets/projects/billboard2.jpg";
 import amrooz1 from "@/assets/image gallery/newspaper.jpeg"
 import amrooz2 from "@/assets/projects/billboard2.jpg"
